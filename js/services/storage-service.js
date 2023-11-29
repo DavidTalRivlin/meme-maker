3029acc3052d449919b7babce9e364a656470096
DavidTalRivlin <David.dave.tal@gmail.com>
2023-11-29 12:06:07 +0200

    First commit

---
+++ js/services/storage-service.js
@@ -0,0 +1,15 @@
+'use strict'
+
+function saveToStorage(key, value) {
+      const strVal = JSON.stringify(value)
+      localStorage.setItem(key, strVal)
+}
+
+function removeFromStorage(key) {
+      localStorage.removeItem(key)
+}
+
+function loadFromStorage(key) {
+      var strVal = localStorage.getItem(key)
+      return JSON.parse(strVal)
+}
